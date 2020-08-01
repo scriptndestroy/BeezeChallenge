4f7369d7bdb48a125e87fe74607404f946b7d0de
scriptndestroy <ojedamaruny@gmail.com>
2020-08-01 19:00:13 +0200

    Add files via upload

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+import { NavMenu } from "./NavMenu";
+// import { NavMenu } from './NavMenu';
+
+export class Layout extends Component {
+  static displayName = Layout.name;
+
+  render() {
+    return (
+      <div>
+        <NavMenu />
+        <Container>{this.props.children}</Container>
+      </div>
+    );
+  }
+}
