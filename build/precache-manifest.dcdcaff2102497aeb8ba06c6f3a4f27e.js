c685f5b69568428a457765d3a148b07d56cdb5d0
scriptndestroy <ojedamaruny@gmail.com>
2020-08-01 19:49:23 +0200

    Add files via upload

---
+++ build/precache-manifest.dcdcaff2102497aeb8ba06c6f3a4f27e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "959a09959f25376cc39c2a0e1c5ab090",
+    "url": "/BeezeChallenge/index.html"
+  },
+  {
+    "revision": "7a750d145dcd6d49f1ba",
+    "url": "/BeezeChallenge/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "178acc6e068b832a8e9e",
+    "url": "/BeezeChallenge/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7a750d145dcd6d49f1ba",
+    "url": "/BeezeChallenge/static/js/2.cf1a731d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/BeezeChallenge/static/js/2.cf1a731d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "178acc6e068b832a8e9e",
+    "url": "/BeezeChallenge/static/js/main.500697e5.chunk.js"
+  },
+  {
+    "revision": "cc57f097a696d2726403",
+    "url": "/BeezeChallenge/static/js/runtime-main.46ad3b0f.js"
+  }
+]);
